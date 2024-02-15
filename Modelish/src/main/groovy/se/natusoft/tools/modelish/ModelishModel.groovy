a791cb696312d0174868a5687795791ebffd80d4
tommy <tommy@natusoft.se>
2024-02-15 11:43:49 +0100

    - Added a desc="..." to ModelishModel for   more consistency with other annotations. - Bumped version to 3.0.13.

--- Modelish/src/main/groovy/se/natusoft/tools/modelish/ModelishModel.groovy
+++ Modelish/src/main/groovy/se/natusoft/tools/modelish/ModelishModel.groovy
@@ -3,4 +3,6 @@ package se.natusoft.tools.modelish
 /**
  * Indicates that an interface actually is a Modelish model. This is purely documentation!
  */
-@interface ModelishModel {}
+@interface ModelishModel {
+    String[] desc();
+}
