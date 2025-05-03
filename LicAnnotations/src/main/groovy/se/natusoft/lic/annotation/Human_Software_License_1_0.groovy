6d247b52a4f371aca9bc6837148a5969934a378d
tommy <tommy@natusoft.se>
2025-05-03 11:28:09 +0200

    Rethinking bad thinking ...
    This does not say that it is now good, but just as good as I can come upp with right now ...

--- LicAnnotations/src/main/groovy/se/natusoft/lic/annotation/Human_Software_License_1_0.groovy
+++ LicAnnotations/src/main/groovy/se/natusoft/lic/annotation/Human_Software_License_1_0.groovy
@@ -5,13 +5,8 @@ import java.lang.annotation.RetentionPolicy
 
 /**
  * This is an annotation for the Human Software License (HSL)!
- * HSL extends the ASL (Apache Software License) To make this
- * very clear this annotation is annotated with @Apache_Software_License_2_0.
- * There is also an "extend" field that have "Apache Software License 2.0"
- * to be very clear! I tried to make one annotation extend
- * another but that failed, failed, and failed.
+ * HSL extends the ASL (Apache Software License)
  */
-@Apache_Software_License_2_0
 @Retention( RetentionPolicy.RUNTIME)
 @interface Human_Software_License_1_0 {
 
@@ -28,5 +23,7 @@ import java.lang.annotation.RetentionPolicy
             "        See the License for the specific language governing permissions and\n" +
             "        limitations under the License.\n";
 
-    String extend() default "Apache Software License 2.0"
+    String URL() default "https://tombensvebloggish.craft.me/hsl"
+
+    Apache_Software_License_2_0 Extends()
 }
