b9f90c4841b7db1dcc9ed25e63841a058b2569e2
tommy <tommy@natusoft.se>
2025-08-29 16:48:23 +0200

    - Removed 'Apache_Software_License_2_0 Extends()' which no longer work for some reason. Removed this field and formated the string information using Groovys """...""", to make it look reasonable in code and that this extends ASF 2.0 is now specified in licence text, which should hopefully print clearly also.
    Bumped version to 1.4.9-BC11.

--- LicAnnotations/src/main/groovy/se/natusoft/lic/annotation/Human_Software_License_1_0.groovy
+++ LicAnnotations/src/main/groovy/se/natusoft/lic/annotation/Human_Software_License_1_0.groovy
@@ -10,20 +10,23 @@ import java.lang.annotation.RetentionPolicy
 @Retention( RetentionPolicy.RUNTIME)
 @interface Human_Software_License_1_0 {
 
-    String licenseText() default "" +
-            "        Licensed under the Human Software License, Version 1.0 (the \"License\");\n" +
-            "        you may not use this file except in compliance with the License.\n" +
-            "        You may obtain a copy of the License at\n" +
-            "\n" +
-            "          https://tombensvebloggish.craft.me/hsl\n" +
-            "\n" +
-            "        Unless required by applicable law or agreed to in writing, software\n" +
-            "        distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-            "        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-            "        See the License for the specific language governing permissions and\n" +
-            "        limitations under the License.\n";
+    String licenseText() default """
+    Licensed under the Human Software License, Version 1.0 (the "License")
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        https://tombensvebloggish.craft.me/hsl
+            
+            Unless required by applicable law or agreed to in writing, software
+            distributed under the License is distributed on an "AS IS" BASIS,
+            WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+            See the License for the specific language governing permissions and
+            limitations under the License.
+            
+        The HSL license extends the Apache Software Licence 2.0.
+    """
 
     String URL() default "https://tombensvebloggish.craft.me/hsl"
 
-    Apache_Software_License_2_0 Extends()
+    
 }
